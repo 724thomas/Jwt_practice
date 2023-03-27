1dfc2e5dbe3554e6e7f21919eaaa8d6fc23025eb
724thomas <724thomas@gmail.com>
2023-03-27 23:48:45 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/exception/AppException.java
+++ src/main/java/com/example/jwt_practice/exception/AppException.java
@@ -1,2 +1,11 @@
-package com.example.jwt_practice.exception;public class AppException {
+package com.example.jwt_practice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AppException extends RuntimeException{
+    private ErrorCode errorCode;
+    private String message;
 }
