81a200799af74c9dd6960d0add59114bb86faa1a
724thomas <724thomas@gmail.com>
2023-03-27 23:48:57 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/configuration/EncoderConfig.java
+++ src/main/java/com/example/jwt_practice/configuration/EncoderConfig.java
@@ -1,2 +1,14 @@
-package com.example.jwt_practice.configuration;public class EncoderConfig {
+package com.example.jwt_practice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncoderConfig { //순환참조 문제가 생길 수 있으므로, encoder, security는 다른 클래스
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
