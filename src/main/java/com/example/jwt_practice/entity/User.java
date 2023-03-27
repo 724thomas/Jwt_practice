4cecc9e42fd4a65aa3a43af210d4254afa9be2aa
724thomas <724thomas@gmail.com>
2023-03-27 23:49:16 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/entity/User.java
+++ src/main/java/com/example/jwt_practice/entity/User.java
@@ -1,2 +1,25 @@
-package com.example.jwt_practice.entity;public class Uuser {
+package com.example.jwt_practice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Table(name = "user")
+@NoArgsConstructor
+@Builder
+@Getter
+public class User {
+    @Id
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+
 }
