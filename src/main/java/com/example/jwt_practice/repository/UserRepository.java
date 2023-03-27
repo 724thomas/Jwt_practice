3f2b99964f7cf19d298c39fe24c6ee8e7f7f1389
724thomas <724thomas@gmail.com>
2023-03-27 23:49:31 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/repository/UserRepository.java
+++ src/main/java/com/example/jwt_practice/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.jwt_practice.repository;public interface UserRepository {
+package com.example.jwt_practice.repository;
+
+import com.example.jwt_practice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
 }
