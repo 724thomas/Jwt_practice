4589666d3d23cda57bff7cf044294043bcd0ebe2
724thomas <724thomas@gmail.com>
2023-03-27 23:49:13 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/controller/ReviewController.java
+++ src/main/java/com/example/jwt_practice/controller/ReviewController.java
@@ -1,2 +1,16 @@
-package com.example.jwt_practice.controller;public class ReviewController {
+package com.example.jwt_practice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+
+    @PostMapping("/review")
+    public ResponseEntity<String> review(Authentication authentication){
+        return ResponseEntity.ok().body(authentication.getName() + "님의 리뷰 등록 성공");
+
+    }
 }
