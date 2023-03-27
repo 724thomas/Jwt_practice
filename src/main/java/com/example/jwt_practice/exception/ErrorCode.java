41ae938997187e597217475d7531d03cd0cd13a5
724thomas <724thomas@gmail.com>
2023-03-27 23:48:59 +0900

    Jwt Template

--- src/main/java/com/example/jwt_practice/exception/ErrorCode.java
+++ src/main/java/com/example/jwt_practice/exception/ErrorCode.java
@@ -1,2 +1,15 @@
-package com.example.jwt_practice.exception;public class ErrorCode {
+package com.example.jwt_practice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "");
+    private HttpStatus httpStatus;
+    private String message;
 }
